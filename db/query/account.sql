a000db63bf8ce04cb9ce7d6a8cb27b8d68025864
xynurm <nyakridha24@gmail.com>
2023-05-23 16:00:11 +0700

    test account CRUD

--- db/query/account.sql
+++ db/query/account.sql
@@ -23,6 +23,6 @@ SET balance = $2
 WHERE id = $1
 RETURNING *;
 
--- name: DeleteAuthor :exec
+-- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1;
--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -38,13 +38,13 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 	return i, err
 }
 
-const deleteAuthor = `-- name: DeleteAuthor :exec
+const deleteAccount = `-- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1
 `
 
-func (q *Queries) DeleteAuthor(ctx context.Context, id int64) error {
-	_, err := q.db.ExecContext(ctx, deleteAuthor, id)
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
 	return err
 }
 
